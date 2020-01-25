63fd5ee5c172cb6fa63d33240aab5e0f36dc05e6
Kamil <kamil.slosarczyk@yahoo.com>
2020-01-25 12:22:19 +0100

    Introduce loading page

--- src/main/java/pl/blackparade/pp5/Application.java
+++ src/main/java/pl/blackparade/pp5/Application.java
@@ -3,9 +3,9 @@ package pl.blackparade.pp5;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-    @SpringBootApplication
-    public class Application{
-        public static void main (String args[]){
-            SpringApplication.run(Application.class, args);
-        }
+@SpringBootApplication
+public class Application{
+    public static void main (String args[]){
+        SpringApplication.run(Application.class, args);
     }
+}
